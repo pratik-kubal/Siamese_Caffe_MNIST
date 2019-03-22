e898a6baa52051dba04dea85b655c10c2bcb9a51
pratik <17088230+pratik-kubal@users.noreply.github.com>
2019-03-22 15:57:23 -0400

    Added Network for simple MNIST

---
+++ create_mnist.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+# This script converts the mnist data into lmdb/leveldb format,
+# depending on the value assigned to $BACKEND.
+set -e
+
+EXAMPLE=./
+DATA=./
+BUILD=/home/pratik/anaconda3/envs/aml/bin/
+
+BACKEND="lmdb"
+
+echo "Creating ${BACKEND}..."
+
+rm -rf $EXAMPLE/mnist_train_${BACKEND}
+rm -rf $EXAMPLE/mnist_test_${BACKEND}
+
+$BUILD/convert_mnist_data $DATA/train-images-idx3-ubyte \
+  $DATA/train-labels-idx1-ubyte $EXAMPLE/mnist_train_${BACKEND} --backend=${BACKEND}
+$BUILD/convert_mnist_data $DATA/t10k-images-idx3-ubyte \
+  $DATA/t10k-labels-idx1-ubyte $EXAMPLE/mnist_test_${BACKEND} --backend=${BACKEND}
+
+echo "Done."
